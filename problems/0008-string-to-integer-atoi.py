e678fd2fe20c8c8f2854e876ccc7032a552057de
Lydia Kwag <lydia.kwag.dev@gmail.com>
2025-06-28 22:12:12 -0400

    Update LeetCode 8 - String to Integer (atoi) - additional comments, core idea, problem description etc.

--- problems/0008-string-to-integer-atoi.py
+++ problems/0008-string-to-integer-atoi.py
@@ -29,31 +29,32 @@
 
 class Solution:
     def myAtoi(self, s: str) -> int:
+        # integer range [-2^31, 2^31 - 1]
         INT_MIN, INT_MAX = -2**31, 2**31 - 1
         i, n = 0, len(s)
         
         # Step 1: Skip leading whitespace
         while i < n and s[i] == ' ':
             i += 1
-        
-        # Step 2: Check if the string is empty after trimming
+        # Check if the string is empty after trimming
         if i == n:
             return 0
         
-        # Step 3: Check for sign
+        # Step 2: Check for sign
         sign = 1
         if s[i] == '+' or s[i] == '-':
             sign = -1 if s[i] == '-' else 1
             i += 1
 
-        # Step 4: Convert digits to integer
+        # Step 3: Convert digits to integer
         num = 0
         while i < n and s[i].isdigit():
             digit = int(s[i])
             
-            # Step 5: Handle overflow/underflow
+            # Step 4: Handle overflow/underflow
             if num > (INT_MAX - digit) // 10:
             # num * 10 + digit > INT_MAX  >> num > (INT_MAX - digit) // 10
+            # doing floor division b/c INT_MAX is an integer
                 return INT_MAX if sign == 1 else INT_MIN
             
             num = num * 10 + digit
