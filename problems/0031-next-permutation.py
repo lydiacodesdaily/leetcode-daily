cd468b8bf27c8b89a4110261bad82dc2ea398802
Lydia Kwag <lydia.kwag.dev@gmail.com>
2025-10-02 22:36:35 -0400

    Add second sln to LeetCode 31 - Next Permutation

--- problems/0031-next-permutation.py
+++ problems/0031-next-permutation.py
@@ -97,3 +97,36 @@ Swap with next biggest on right
 Reverse the right side
 
 """
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # start from the right
+        # find the dip = pivot point 
+        i = len(nums) - 2
+        while i >= 0:
+            if nums[i] < nums[i+1]:
+                break
+            else:
+                i -= 1 
+                
+        # from the right, find the # greater than pivot 
+        # if found, swap 
+        if i >= 0:
+            j = len(nums) - 1 
+            while j > i:
+                if nums[j] > nums[i]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    break
+                j -= 1 
+        
+        # reverse the # from the the pivot 
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1 
+            right -= 1 
+        
+        # T: O(n), S: O(1)
